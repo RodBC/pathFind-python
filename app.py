f1b602b457800c07e00c9fc0b6de5f3281e3309d
RodBC <rbc6@cin.ufpe.br>
2022-09-24 14:32:13 -0300

    adding the data taken from the selected db to the screen

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
-@app.route('/')
-def index():
+def getData():
     tasks = []
     with open("Dados.txt", "r") as arquivo:
         for line in arquivo:
             origem, destino, peso = line.split()
             if origem not in tasks:
                 tasks.append(origem)
-                print(tasks)
-    return render_template('index.html', task=tasks)
+    return tasks
+@app.route('/')
+def index():                
+    return render_template('index.html', tasks=getData())
 
 
 app.run(debug=True)
